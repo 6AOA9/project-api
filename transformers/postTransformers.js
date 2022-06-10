1037f140dc84aca2d8554fc74f57859125104c3d
Feras Jobeir <fjobeir@gmail.com>
2022-06-10 22:58:30 +0300

    delete user password from response

--- transformers/postTransformers.js
+++ transformers/postTransformers.js
@@ -1,5 +1,10 @@
+const { userTransformer } = require('./userTransformers')
+
 const postTransformer = (post) => {
     post.picture = `${process.env.URL + '' + process.env.UPLOADS + '' + post.picture}`
+    if (post.User) {
+        post.User = userTransformer(post.User)
+    }
     return post;
 }
 const postsTransformer = (ArrayOfposts) => {
--- transformers/userTransformers.js
+++ transformers/userTransformers.js
@@ -1,17 +1,6 @@
-// const userTransformers = (user) => {
-//     user.profilePic = `${process.env.URL + process.env.UPLOADS + user.profilePic}`
-//     return user;
-// }
-// const usersTransformers = (ArrayOfusers) => {
-//     return ArrayOfusers.map((singleuser) => userTransformers(singleuser))
-// };
-// module.exports = {
-//     userTransformers,
-//     usersTransformers
-// }
-
 const userTransformer = (user) => {
-    delete user['dataValues']['password'];
+    delete user?.['dataValues']?.['password'];
+    delete user?.['password'];
     return user;
 }
 
